ec950170b60cb237593bc1be1d597b3d5509c210
gkdlvjdhrkaw <gkdlvjdhrkaw@gmail.com>
2021-12-21 12:55:31 +0900

    [21.12.21] # 자바 클래스 객체 this 와 예제

---
+++ src/day11/Test.java
@@ -0,0 +1,19 @@
+package day11;
+
+//import day11.fruit.Banana;
+//import day11.fruit.Grape;
+
+import day11.fruit.*;  //  * 은 모두 라는 뜻
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        day11.syrub.Apple a1 = new day11.syrub.Apple();
+        day11.fruit.Apple a2 = new day11.fruit.Apple();
+        day11.fruit.Banana b1 = new day11.fruit.Banana();
+
+        Banana b11 = new Banana();
+        day11.fruit.Grape g1 = new Grape();
+    }
+}
---
+++ src/day11/syrub/Apple.java
@@ -0,0 +1,12 @@
+package day11.syrub;
+// .()() 쓰고 Alt + Enter 하면 패키지 속 패키지
+
+
+public class Apple {
+
+    public Apple() {
+        System.out.println("사과쥬스 시럽입니다.");
+
+
+    }
+}
